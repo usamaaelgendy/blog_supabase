d4295e76feb501c2e4abe8a19d1c8eaaf22fca56
Usama Elgendy <oelgindy@moj.gov.sa>
2026-04-03 09:49:11 +0200

    remove unused import of post_model in post_query_datasource_impl.dart

--- lib/features/blog/data/datasources/post_query_datasource_impl.dart
+++ lib/features/blog/data/datasources/post_query_datasource_impl.dart
@@ -4,7 +4,6 @@ import 'package:blog_app/features/blog/data/datasources/post_query_datasource.da
 import 'package:blog_app/features/blog/data/models/post_model.dart';
 
 class PostQueryDataSourceImpl implements PostQueryDataSource {
-  // ignore: unused_field
   final DatabaseClient _databaseClient;
 
   PostQueryDataSourceImpl(this._databaseClient);
@@ -13,13 +12,6 @@ class PostQueryDataSourceImpl implements PostQueryDataSource {
   Future<List<PostModel>> getPosts({int? rangeFrom, int? rangeTo}) async {
     try {
       // TODO: Implement getPosts
-      // Use _databaseClient.select() with:
-      //   table: 'posts'
-      //   columns: '*, author:profiles!author_id(*)'
-      //   orderBy: 'created_at'
-      //   ascending: false
-      //   rangeFrom and rangeTo for pagination
-      // Map each result to PostModel.fromJson()
       throw UnimplementedError('getPosts not implemented yet');
     } on ServerException {
       rethrow;
@@ -32,12 +24,6 @@ class PostQueryDataSourceImpl implements PostQueryDataSource {
   Future<List<PostModel>> getMyPosts({required String userId, String? category}) async {
     try {
       // TODO: Implement getMyPosts
-      // Use _databaseClient.select() with:
-      //   table: 'posts'
-      //   columns: '*, author:profiles!author_id(*)'
-      //   filters: {'author_id': userId} (and optionally 'category': category)
-      //   orderBy: 'created_at'
-      // Map each result to PostModel.fromJson()
       throw UnimplementedError('getMyPosts not implemented yet');
     } on ServerException {
       rethrow;
@@ -50,12 +36,6 @@ class PostQueryDataSourceImpl implements PostQueryDataSource {
   Future<List<PostModel>> searchPosts({required String query}) async {
     try {
       // TODO: Implement searchPosts
-      // Use _databaseClient.search() with:
-      //   table: 'posts'
-      //   column: 'title'
-      //   query: query
-      //   columns: '*, author:profiles!author_id(*)'
-      // Map each result to PostModel.fromJson()
       throw UnimplementedError('searchPosts not implemented yet');
     } on ServerException {
       rethrow;
